46d3189376aa87a0c33d131f7c23933317d49a49
K0stya666 <144836256+K0stya666@users.noreply.github.com>
2023-09-22 19:02:11 +0300

    Add files via upload

---
+++ №11.1.py
@@ -0,0 +1,25 @@
+def Zeck_sys(A):
+    ryad_fib = [1, 1]
+    while ryad_fib[-1] < A:
+        ryad_fib.append(ryad_fib[-1] + ryad_fib[-2])
+    ryad_fib = [ryad_fib[x] for x in range(len(ryad_fib)-1,0,-1) if ryad_fib[x] <= A]
+    res = ''
+    s = 0
+    for i in ryad_fib:
+        if A >= s + i:
+            s+=i
+            res += '1'
+        else:
+            res+='0'
+
+    return res
+
+A = 144
+B = 10
+C = 'Fib'
+
+print(Zeck_sys(A))
+
+A = int(input())
+
+print(Zeck_sys(A))
