4a39ce5f29036b2615efd74a0144380eadbaf174
K0stya666 <144836256+K0stya666@users.noreply.github.com>
2023-09-23 13:43:25 +0300

    Update №11.1.py

--- №11.1.py
+++ №11.1.py
@@ -1,3 +1,16 @@
+def f(A):
+    A = str(A)
+    if A.count('1')+A.count('0')!=len(A): return None
+    ryad_fib = [1, 1]
+    for i in range(len(A)):
+        ryad_fib.append(ryad_fib[-1] + ryad_fib[-2])
+    A = A[-1::-1]
+    res = 0
+    ryad_fib=ryad_fib[1:]
+    for i in range(len(A)-1,-1,-1):
+        res += ryad_fib[i] * int(A[i])
+    return res
+
 def Zeck_sys(A):
     if A == 0: return 0
     ryad_fib = [1, 1]
@@ -19,8 +32,13 @@ A = 144
 B = 10
 C = 'Fib'
 
-print("Ответ на задание 11:",Zeck_sys(A))
-print("Введите число")
+print("Ответ на задание 11 из десятичной в фиб:",Zeck_sys(A))
+print("Введите число ")
 A = int(input())
 
 print(Zeck_sys(A))
+
+B = 10000000000
+print("Ответ на задание 11 из фиб в десятичную:",f(B))
+B = int(input())
+print(f(B))
